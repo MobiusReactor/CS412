45f141415652e2954cdc18f2ab6c81a2cb1238fd
Scott Johnston <mobiusreactor@gmail.com>
2016-11-11 15:07:54 +0000

    Changed advanced search panel a bit, added basic interactivity

--- src/gui/AdvancedSearchPanel.java
+++ src/gui/AdvancedSearchPanel.java
@@ -1,39 +1,70 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class AdvancedSearchPanel extends JPanel {
+public class AdvancedSearchPanel extends JPanel implements ActionListener {
+	private JComboBox<String> typeBox;
+	private JLabel characterLabel;
+	private JTextField searchBox, playBox, characterBox;
+	private JButton searchButton;
+
 	public AdvancedSearchPanel() {
-		JLabel l = new JLabel("Enter Search Term");
-		add(l);
+		JLabel searchLabel = new JLabel("Enter Search Term");
+		add(searchLabel);
 
-		JTextField searchBox = new JTextField(15);
+		searchBox = new JTextField(15);
 		add(searchBox);
-
-		JLabel typeLabel = new JLabel("Type");
-		add(typeLabel);
-
-		String[] targetType = { "All", "Titles", "Dialogue", "Stage Directions" };
-		JComboBox<String> typeBox = new JComboBox<>(targetType);
-		add(typeBox);
 
 		JLabel playLabel = new JLabel("Play");
 		add(playLabel);
 
-		JTextField playBox = new JTextField(15);
+		playBox = new JTextField(15);
 		add(playBox);
 
-		JLabel characterLabel = new JLabel("Character");
-		add(characterLabel);
+		JLabel typeLabel = new JLabel("Type");
+		add(typeLabel);
 
-		JTextField characterBox = new JTextField(15);
-		add(characterBox);
+		String[] targetType = { "All", "Scene Titles", "Dialogue", "Stage Directions" };
+		typeBox = new JComboBox<>(targetType);
+		typeBox.addActionListener(this);
+		add(typeBox);
 
-		JButton b = new JButton("Search");
-		add(b);
+		characterLabel = new JLabel("Character");
+		characterBox = new JTextField(15);
+
+		searchButton = new JButton("Search");
+		searchButton.addActionListener(this);
+		add(searchButton);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == typeBox) {
+			if (typeBox.getSelectedItem().equals("Dialogue")) {
+				remove(searchButton);
+				add(characterLabel);
+				add(characterBox);
+				add(searchButton);
+				repaint();
+			} else {
+				remove(characterLabel);
+				remove(characterBox);
+				repaint();
+			}
+		} else if (e.getSource() == searchButton) {
+			if (typeBox.getSelectedItem().equals("Dialogue")) {
+				System.out.println("Searching for \"" + searchBox.getText() + "\" in Dialogue fields of the play \"" + playBox.getText() + "\", spoken by " + characterBox.getText());
+			} else {
+				System.out.println("Searching for \"" + searchBox.getText() + "\" in " + typeBox.getSelectedItem() + " fields of the play \"" + playBox.getText() + "\"");
+			}
+			
+			// Call actual search method here
+		}
 	}
 }
